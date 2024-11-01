1d435ec5745085c9ae8bebbcf50be9a4c2c2ee3b
mraihanhatim <raihan04042001@gmail.com>
2024-11-01 10:58:26 +0700

    add search button in navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { GemIcon, Search } from "lucide-react";
+import { GemIcon, SearchIcon } from "lucide-react";
 import Link from "next/link";
 import { Input } from "../../components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   return (
     <nav className="flex p-3 bg-background border border-black shadow-md">
       <div className="flex container lg:mx-10 justify-between">
-        <div className="flex ">
+        <div className="flex items-center">
           <Link href="/">
-            <h1 className="text-3xl font-bold">LandAnime</h1>
+            <h1 className="text-3xl font-bold mr-2">LandAnime</h1>
           </Link>
           <GemIcon size={34} />
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Search Anime..."
+              className="pr-10 px-2"
+            />
+            <button className="absolute inset-y-0 right-2 flex items-center">
+              <SearchIcon size={25} />
+            </button>
+          </div>
         </div>
       </div>
     </nav>
